929c3cb8bf1a378574741bfea60e80af7fc61eb3
chocho77 <chavmom@gmail.com>
2025-05-03 11:06:14 +0300

    read this first

---
+++ stringsmethods/readme_this_script.py
@@ -0,0 +1,107 @@
+#Here's a list of all standard string methods available in Python 3.12:
+#String Methods in Python 3.12
+#Query / Check Methods
+
+#    str.isalnum()
+
+#    str.isalpha()
+
+#    str.isascii()
+
+#    str.isdecimal()
+
+#    str.isdigit()
+
+#    str.isidentifier()
+
+#    str.islower()
+
+#    str.isnumeric()
+
+#    str.isprintable()
+
+#    str.isspace()
+
+#    str.istitle()
+
+#    str.isupper()
+
+#    str.startswith(prefix[, start[, end]])
+
+#    str.endswith(suffix[, start[, end]])
+
+# Search / Indexing Methods
+
+#    str.find(sub[, start[, end]])
+
+#    str.rfind(sub[, start[, end]])
+
+#    str.index(sub[, start[, end]])
+
+#    str.rindex(sub[, start[, end]])
+
+#    str.count(sub[, start[, end]])
+
+#Case Conversion
+
+#    str.capitalize()
+
+#    str.lower()
+
+#    str.upper()
+
+#    str.title()
+
+#    str.swapcase()
+
+#    str.casefold()
+
+#Modify / Format
+
+#    str.strip([chars])
+
+#    str.lstrip([chars])
+
+#    str.rstrip([chars])
+
+#    str.replace(old, new[, count])
+
+#    str.zfill(width)
+
+#    str.removeprefix(prefix) (Python 3.9+)
+
+#    str.removesuffix(suffix) (Python 3.9+)
+
+#    str.translate(table)
+
+#    str.expandtabs([tabsize])
+
+#vAlignment
+
+#    str.center(width[, fillchar])
+
+#    str.ljust(width[, fillchar])
+
+#    str.rjust(width[, fillchar])
+
+#Split / Join
+
+#    str.split([sep[, maxsplit]])
+
+#    str.rsplit([sep[, maxsplit]])
+
+#    str.splitlines([keepends])
+
+#    str.partition(sep)
+
+#    str.rpartition(sep)
+
+#    str.join(iterable)
+
+#Encoding / Formatting
+
+#    str.encode(encoding="utf-8", errors="strict")
+
+#    str.format(*args, **kwargs)
+
+#    str.format_map(mapping)
