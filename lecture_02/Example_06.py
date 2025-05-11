94bd91091c3252d46ffb9cacc511e6b8ecdd9dc3
chocho77 <chavmom@gmail.com>
2025-05-11 16:52:12 +0300

    continue with lecture

--- lecture_02/Example_06.py
+++ lecture_02/Example_06.py
@@ -26,8 +26,42 @@ print(1, 2, 3, sep="!")
 print(1, 2, 3, end="!!!!!") # print with  an end
 print(1, 2, 3, end="!!!!!\n")
 print("Done")
-print(1, 2 ,3, sep="\"some text\"", end="") # print with both
+print(1, 2 ,3, sep="\"some text\"", end="\n") # print with both
 x = 123456789011111123456789011111 # Long integer
+y = 123456789011111123456789011111 # Long integer
+print(f"x = {x}")
+print(f"y = {y}")
+print(f"x + y = {x+y}") # add long integer
+print(f"x * y = {x * y}") # multiply long integer
+text = "\"Hello\""
+print(text) # Escape
+tab = "\t" # use tab
+print(f"{tab} {text}") # tab
+text = '"Hello\n\n\n"'
+print(text)
+text = '"Hello\n\n\n\t"'
+print(text)
+result_add_numbers = 3 + 4
+print(result_add_numbers)
+result_sub_numbers = 5 - 4
+print(result_sub_numbers)
+result_multy_numbers = 4 * 3
+print(result_multy_numbers)
+result_divide_numbers = 4 / 3 # Float division
+print(result_divide_numbers)
+result_whole_divide_numbers = 4 // 3 # Integer division
+print(result_whole_divide_numbers)
+x = 6
+y = 8
+result_pow = x ** y
+print(f"{x} ** {y} = {result_pow}")
+
+print(5 % 3) # division with remain : remain is 2
+
+
+
+
+
 
 
 
