dbb2b5b5d6a1c1510651ac4bcdf7bb0f1f4adc67
chocho77 <chavmom@gmail.com>
2025-05-11 17:12:29 +0300

    comparison operators

--- lecture_02/Example_07.py
+++ lecture_02/Example_07.py
@@ -12,4 +12,39 @@ if x < y: # True
 else:
     print("False")
 
+t = 4
+z = 4
+
+if t == z: #True
+    print("True")
+else:
+    print("False")
+
+
+if t != z: #False
+    print("True")
+else:
+    print("False")
+
+
+d = 5
+g = 2
+
+if d >= g: #True
+    print("True")
+
+if d <= g: # False
+    print("True")
+else:
+    print("False")
+
+h = 5
+f = 5
+
+if h <= f: # True
+    print("True")
+
+if h >= f: # True
+    print("True")
+
 
