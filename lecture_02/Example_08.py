4c1cbc63d4664432c7dcac6a4fe43b4db04b33cf
chocho77 <chavmom@gmail.com>
2025-05-12 17:32:59 +0300

    escape symbols and logic operators

---
+++ lecture_02/Example_08.py
@@ -0,0 +1,22 @@
+text = "'Hello'" # 'Hello'
+print(text)
+text_escape = "\"Hello\"" # "Hello"
+print(text_escape)
+
+x = 5
+print(x < 6) # True
+
+x = 6
+t = 7
+
+print(x > 5 and t > 6) # True
+
+print(x < 6 < t) # False
+
+print(x > t or x > 5) # True
+
+x = 25
+
+print(20 < x < 50) # True
+
+
